c027404649776d2ceb9d8591dd64b5a89e8d5a35
Yevhen Tienkaiev <hronom@gmail.com>
2025-04-06 05:28:12 +0300

    #10 Add UserSettingsView component for managing user settings

---
+++ src/views/UserSettingsView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h1 class="text-start my-1">Dashboard</h1>
+    <p v-if="user" class="text-start my-1">Welcome back, email: {{ user.email }}</p>
+    <button class="btn btn-primary my-1" @click="test">Test</button>
+    <p v-if="testData" class="text-start my-1">Test data: {{ testData }}</p>
+  </div>
+</template>
+
+<script>
+import {authronomBackendAuthAxiosInstance} from "@/services/axios.js";
+import {useAuthStore} from '@/services/auth';
+
+export default {
+  data() {
+    return {
+      user: null,
+      testData: null
+    };
+  },
+
+  mounted() {
+    const authStore = useAuthStore();
+    this.user = authStore.getUser;
+  },
+
+  methods: {
+    async test() {
+      try {
+        const authStore = useAuthStore();
+        const response = await authronomBackendAuthAxiosInstance.get('/auth/test', {});
+        this.testData = response.data;
+      } catch (error) {
+        console.error("Error fetching test data:", error);
+      }
+    }
+  }
+};
+</script>
